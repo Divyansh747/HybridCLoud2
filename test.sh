4656a3ac8a16d28ccd258d2cc4b2eb92e899046d
Divyansh-747 <divyansh.1999.dr@gmail.com>
2020-07-20 03:33:47 +0530

    remove extra /

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "## adding script in index.html ##"
-sudo echo "<script>\$(function(){\$(\"img[src='']\").attr(\"src\", 'http:///$(cat cloudfront_link.txt)');});</script>">> /var/www/html/index.html
+sudo echo "<script>\$(function(){\$(\"img[src='']\").attr(\"src\", 'http://$(cat cloudfront_link.txt)');});</script>">> /var/www/html/index.html
 echo "## index.html successfully updated ##"
 echo "## restarting httpd service ##"
 sudo systemctl restart httpd
