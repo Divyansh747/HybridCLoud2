a288588c9f9fc5ef77357acb287c1a15070ce723
Divyansh-747 <divyansh.1999.dr@gmail.com>
2020-07-20 03:04:58 +0530

    Ingoring first time ssh (yes/no) promt

--- testrun.sh
+++ testrun.sh
@@ -1,7 +1,7 @@
 chmod 400 /root/HybridCLoud2/tls_key.pem
 
-scp -i   /root/HybridCLoud2/tls_key.pem -o "CheckHostIP=no" /root/HybridCLoud2/test.sh  ec2-user@$(cat /root/HybridCLoud2/ec2_link.txt):/home/ec2-user/
-scp -i   /root/HybridCLoud2/tls_key.pem -o "CheckHostIP=no" /root/HybridCLoud2/cloudfront_link.txt  ec2-user@$(cat /root/HybridCLoud2/ec2_link.txt):/home/ec2-user/
+scp -i   /root/HybridCLoud2/tls_key.pem -o "StrictHostKeyChecking no" /root/HybridCLoud2/test.sh  ec2-user@$(cat /root/HybridCLoud2/ec2_link.txt):/home/ec2-user/
+scp -i   /root/HybridCLoud2/tls_key.pem -o "StrictHostKeyChecking no" /root/HybridCLoud2/cloudfront_link.txt  ec2-user@$(cat /root/HybridCLoud2/ec2_link.txt):/home/ec2-user/
 
-ssh -tt -i /root/HybridCLoud2/tls_key.pem -o "CheckHostIP=no" ec2-user@$(cat /root/HybridCLoud2/ec2_link.txt) "chmod 777 test.sh; ./test.sh"
+ssh -tt -i /root/HybridCLoud2/tls_key.pem -o "StrictHostKeyChecking no" ec2-user@$(cat /root/HybridCLoud2/ec2_link.txt) "chmod 777 test.sh; ./test.sh"
 
