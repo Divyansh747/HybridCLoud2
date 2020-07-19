13cb90c8e34cb315f7b59341011494b78389c7f8
Divyansh-747 <divyansh.1999.dr@gmail.com>
2020-07-20 00:08:21 +0530

    add security group

--- main.tf
+++ main.tf
@@ -23,3 +23,43 @@ resource "aws_key_pair" "tls_key" {
   key_name        = "tls_key"
   public_key      = tls_private_key.tls_key.public_key_openssh
 }
+
+#security group
+resource "aws_security_group" "ssh-http-1" {
+  depends_on  = [aws_key_pair.tls_key]
+  name        = "ssh-http"
+  description = "allow ssh and http"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+ tags = {
+   Name = "sg2"
+ }
+}
