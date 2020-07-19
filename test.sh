b197c93ffa391895ad9734e4b36c7b0234c0c5b7
Divyansh-747 <divyansh.1999.dr@gmail.com>
2020-07-20 00:11:18 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "## adding script in index.html ##"
+sudo echo "<script>\$(function(){\$(\"img[src='']\").attr(\"src\", 'http:///$(cat cloudfront_link.txt)');});</script>">> /var/www/html/index.html
+echo "## index.html successfully updated ##"
+echo "## restarting httpd service ##"
+sudo systemctl restart httpd
+echo "## httpd successfully restarted  ##"
+echo "## exiting script... ##"
